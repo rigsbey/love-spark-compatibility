3f35a30dbd4b6b8f836c0d63ddb0b79b8ca4e1a8
kamil <k.abulkhanov@yallamart.ae>
2025-01-27 21:14:46 +0100

    Setup GitHub Pages deployment

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,12 @@
-import { Toaster } from "@/components/ui/toaster";
-import { Toaster as Sonner } from "@/components/ui/sonner";
-import { TooltipProvider } from "@/components/ui/tooltip";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Index from "./pages/Index";
+import { Routes, Route } from 'react-router-dom'
+import Index from './pages/Index'
 
-const queryClient = new QueryClient();
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Index />} />
+    </Routes>
+  )
+}
 
-const App = () => (
-  <QueryClientProvider client={queryClient}>
-    <TooltipProvider>
-      <Toaster />
-      <Sonner />
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Index />} />
-        </Routes>
-      </BrowserRouter>
-    </TooltipProvider>
-  </QueryClientProvider>
-);
-
-export default App;
+export default App
