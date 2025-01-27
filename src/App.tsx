161c5cd5740814137a411638d66f9079ea0e9025
kamil <k.abulkhanov@yallamart.ae>
2025-01-27 21:16:46 +0100

    Setup GitHub Pages deployment

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { Routes, Route } from 'react-router-dom'
 import Index from './pages/Index'
 
 function App() {
+  console.log('App component mounted');
   return (
     <Routes>
       <Route path="/" element={<Index />} />
--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter as Router } from 'react-router-dom'
+import { HashRouter as Router } from 'react-router-dom'
 import App from './App'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <Router basename="/love-spark-compatibility">
+    <Router>
       <App />
     </Router>
   </React.StrictMode>,
